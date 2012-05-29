a1f30d2c6241f5cfd59f0c346e15c2b3dab6f884
Guy Fischman <guydevs@gmail.com>
2012-05-29 11:01:25 -0700

    distance matrix script

---
+++ distances.py
@@ -0,0 +1,29 @@
+from sys import argv
+import urllib
+import json
+
+def main():
+    script, filein, fileout = argv
+    
+    baseurl = 'http://maps.googleapis.com/maps/api/distancematrix/json?'
+    
+    coords = open(filein).readlines()
+    coords_distance_time = open(fileout, 'w')
+    
+    for origin in coords:
+        for destination in coords:
+            disttime = json.load(urllib.urlopen(baseurl + 'origins=' + 
+                       origin[:-1] + '&destinations=' + destination[:-1] + 
+                       '&sensor=false'))
+
+            distance = disttime['rows'][0]['elements'][0]['distance']['value']
+            time = disttime['rows'][0]['elements'][0]['duration']['value']
+
+            coords_distance_time.write('{},{},{},{}\n'.format(
+            origin[:-1], destination[:-1], distance, time))
+
+            print '{},{},{},{}'.format(
+                  origin[:-1], destination[:-1], distance, time)
+            
+if __name__ == '__main__':
+    main()
